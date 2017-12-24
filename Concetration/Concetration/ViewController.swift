f81aa06367b40a3804e1e1bda5696994f6811891
olharash <olha.tretiak.ua@gmail.com>
2017-12-24 20:57:19 +0200

    lection1-added card button UI; implemented flip cards method

---
+++ Concetration/Concetration/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Concetration
+//
+//  Created by Olga on 12/24/17.
+//  Copyright © 2017 olharash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiCHoices:Array <String> = ["🎃","👻","🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiCHoices[cardNumber], on: sender)
+        } else {
+            print("choosen card was not in cardButtons")
+        }
+        
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
